a9479941f9bc77b6e8fb1d0d046c1920bf28e9db
Valay0429 <a49986661@gmail.com>
2026-09-02 19:03:28 +0900

    bullet tracer

---
+++ Assets/Member/JJK/02. Scripts/Weapon/BulletTracerModule.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using DevLib.ModuleSystem;
+using UnityEngine;
+
+namespace Member.JJK._02._Scripts.Weapon
+{
+    public class BulletTracerModule : MonoBehaviour, IModule
+    {
+        [SerializeField] private float tracerDuration = 0.05f;
+        
+        private LineRenderer _lineRenderer;
+        private float _timer;
+        private bool _isActive;
+        
+        public void Initialize(ModuleOwner owner)
+        {
+            _lineRenderer = GetComponent<LineRenderer>();
+            _lineRenderer.enabled = false;
+        }
+        
+        public void ShowTracer(Vector3 start, Vector3 end)
+        {
+            _lineRenderer.SetPosition(0, start);
+            _lineRenderer.SetPosition(1, end);
+            _lineRenderer.enabled = true;
+            _timer = tracerDuration;
+            _isActive = true;
+        }
+        
+        private void FixedUpdate()
+        {
+            if (!_isActive) return;
+
+            _timer -= Time.fixedDeltaTime;
+            float t = Mathf.Clamp01(_timer / tracerDuration);
+
+            var color = _lineRenderer.startColor;
+            color.a = t;
+            _lineRenderer.startColor = color;
+            _lineRenderer.endColor = new Color(color.r, color.g, color.b, 0f);
+            
+            if (_timer <= 0f)
+            {
+                _lineRenderer.enabled = false;
+                _isActive = false;
+            }
+        }
+    }
+}
--- Assets/Member/JJK/02. Scripts/Weapon/ShootModule.cs
+++ Assets/Member/JJK/02. Scripts/Weapon/ShootModule.cs
@@ -5,9 +5,13 @@ namespace Member.JJK._02._Scripts.Weapon
 {
     public class ShootModule : MonoBehaviour, IModule
     {
+        [SerializeField] private Camera playerCam;
+        [SerializeField] private Transform muzzleTrm;
+        
         private WeaponSO _weaponData;
         private AmmoRuntimeState _ammoState;
         private AimModule _aimModule;
+        private BulletTracerModule _tracerModule;
         private float _lastFireTime;
         
         public void Initialize(ModuleOwner owner)
@@ -15,6 +19,7 @@ namespace Member.JJK._02._Scripts.Weapon
             _weaponData = owner.GetComponent<WeaponController>().WeaponData;
             _ammoState = new AmmoRuntimeState(_weaponData);
             _aimModule = owner.GetModule<AimModule>();
+            _tracerModule = owner.GetModule<BulletTracerModule>();
         }
 
         public void TryFire()
@@ -29,13 +34,25 @@ namespace Member.JJK._02._Scripts.Weapon
             
             _lastFireTime = Time.time;
             _ammoState.currentAmmo--;
-            //발사 로직
-            _aimModule.ApplyRecoil(_weaponData.Recoil);
+            FireRayCast();
         }
 
         private void FireRayCast()
         {
+            Ray ray = playerCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+            Vector3 tracerEndPoint;
+
+            if (Physics.Raycast(ray, out RaycastHit hit, 100f))
+            {
+                tracerEndPoint = hit.point;
+            }
+            else
+            {
+                tracerEndPoint = ray.origin + ray.direction * 100f;
+            }
             
+            _tracerModule.ShowTracer(muzzleTrm.position, tracerEndPoint);
+            _aimModule.ApplyRecoil(_weaponData.Recoil);
         }
 
         private void SpawnMuzzleFlash()
