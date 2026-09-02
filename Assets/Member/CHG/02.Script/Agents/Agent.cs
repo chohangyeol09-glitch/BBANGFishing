1da647161a009ac11b6462a7926360dea440a9b2
chohangyeol09 <chohangyeol09@gmail.com>
2026-09-02 19:02:53 +0900

    feat : test Rod data, damage, fish data

--- Assets/Member/CHG/02.Script/Agents/Agent.cs
+++ Assets/Member/CHG/02.Script/Agents/Agent.cs
@@ -1,25 +1,31 @@
-﻿using DevLib.ModuleSystem;
+﻿using System;
+using CHG._02.Script.CombatSystem;
+using DevLib.ModuleSystem;
 using UnityEngine;
 
-namespace Member.CHG._02.Script.Agents
+namespace CHG._02.Script.Agents
 {
     public abstract class Agent : ModuleOwner, IDamageable
     {
+        public event Action OnDeaded;
+        public event Action<DamageData> OnDamaged;
+        
         public float CurrentHealth
         {
             get => _currentHealth;
-            set
+            protected set
             {
-                _currentHealth = Mathf.Max(value, 0f);
+                _currentHealth = Mathf.Clamp(value, 0, MaxHealth);
                 if (_currentHealth <= 0f)
                     Dead();
 
             }
         }
-        public float MaxHealth => _maxHealth;
+
+        public virtual float MaxHealth => _maxHealth;
         
         private float _currentHealth;
-        private float _maxHealth;
+        [SerializeField] protected float _maxHealth = 100f;
         public bool IsDead => _currentHealth <= 0f;
 
         protected override void InitializeModules()
@@ -28,19 +34,21 @@ namespace Member.CHG._02.Script.Agents
             _currentHealth = MaxHealth;
         }
 
-        public virtual void TakeDamage(float damage)
+        public virtual void TakeDamage(DamageData data)
         {
-            CurrentHealth -= damage;
+            if (IsDead) return;
+            CurrentHealth -= data.Damage;
+            OnDamaged?.Invoke(data);
         }
-
+        
         public void Heal(float heal)
         {
             CurrentHealth += heal;
         }
 
-        public void Dead()
+        public virtual void Dead()
         {
-            
+            OnDeaded?.Invoke();
         }
     }
 }
